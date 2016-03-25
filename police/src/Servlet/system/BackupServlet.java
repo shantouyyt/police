ecb27128f206539a4349e643d03c3479c0867e53
shantouyyt <shantouyyt@163.com>
2016-03-25 17:23:52 +0800

    备份

---
+++ police/src/Servlet/system/BackupServlet.java
@@ -0,0 +1,94 @@
+package Servlet.system;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.Result;
+
+import com.google.gson.Gson;
+
+public class BackupServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public BackupServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		Result oret = Result.Fail("");
+		Gson gson = new Gson();
+
+		String act = request.getParameter("act");
+		if ("backup".equals(act)) {
+			
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
--- police/src/Utils/BackupMysql.java
+++ police/src/Utils/BackupMysql.java
@@ -8,14 +8,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+import DBManager.BackupConfig;
+
 public class BackupMysql {
 	public static void backup() {
 		try {
 			Runtime rt = Runtime.getRuntime();
 			// 调用 调用mysql的安装目录的命令
 
-			Process child = rt
-					.exec("E://mysql//mysql-5.6.22-winx64//bin//mysqldump -h localhost -uroot -p123456  police");
+			Process child = rt.exec(BackupConfig.MYSQL_BASE_BIN
+					+ "mysqldump -h " + BackupConfig.IPADDRESS + " -"
+					+ BackupConfig.USERNAME + " -p" + BackupConfig.PASSWORD
+					+ "  " + BackupConfig.BACKUPDB + "");
 
 			// 设置导出编码为utf-8。这里必须是utf-8
 			// 把进程执行中的控制台输出信息写入.sql文件，即生成了备份文件。注：如果不对控制台信息进行读出，则会导致进程堵塞无法运行
@@ -32,7 +36,7 @@ public class BackupMysql {
 			}
 			outStr = sb.toString();
 			// 要用来做导入用的sql目标文件：
-			FileOutputStream fout = new FileOutputStream("c:/test.sql");
+			FileOutputStream fout = new FileOutputStream(BackupConfig.BACKFILE);
 			OutputStreamWriter writer = new OutputStreamWriter(fout, "utf-8");
 			writer.write(outStr);
 			writer.flush();
@@ -49,12 +53,14 @@ public class BackupMysql {
 
 	public static void load() {
 		try {
-			String fPath = "c:/test.sql";
+			String fPath = BackupConfig.BACKFILE;
 			Runtime rt = Runtime.getRuntime();
 
 			// 调用 mysql 的 cmd:
-			Process child = rt
-					.exec("E://mysql//mysql-5.6.22-winx64//bin//mysql.exe -h localhost -uroot -p123456  police ");
+			Process child = rt.exec(BackupConfig.MYSQL_BASE_BIN
+					+ "mysql.exe -h " + BackupConfig.IPADDRESS + " -u"
+					+ BackupConfig.USERNAME + " -p" + BackupConfig.PASSWORD
+					+ "  " + BackupConfig.BACKUPDB + " ");
 			OutputStream out = child.getOutputStream();// 控制台的输入信息作为输出流
 			String inStr;
 			StringBuffer sb = new StringBuffer("");
