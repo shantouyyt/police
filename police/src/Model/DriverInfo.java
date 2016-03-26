c6dd19f3ba4a34ef62e550f4e1fa6ef477a19b9b
Administrator <Administrator@ASUS-PC>
2016-03-26 09:37:49 +0800

    sql

---
+++ police/src/Model/DriverInfo.java
@@ -0,0 +1,81 @@
+package Model;
+
+import com.google.gson.Gson;
+
+public class DriverInfo {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getLicense() {
+		return license;
+	}
+	public void setLicense(String license) {
+		this.license = license;
+	}
+	public String getLicenseExpire() {
+		return licenseExpire;
+	}
+	public void setLicenseExpire(String licenseExpire) {
+		this.licenseExpire = licenseExpire;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	private int id;
+	private String name;
+	private	int sex;
+	private String tel;
+	private String license;
+	private String licenseExpire;
+	private String address;
+	private String createDate;
+	private String endDate;
+	private String remark;
+	
+	public String toString()
+	{
+		return new Gson().toJson(this);
+	}
+}
