8a993faaa838161757f88f4295258db14bf18bf7
Administrator <Administrator@ASUS-PC>
2016-03-31 20:32:01 +0800

    aa

---
+++ police/src/Model/Evidence.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Evidence {
+	private int id;
+	private String accidentNO;
+	private String sGDD;
+	private String tQ;
+	private String qSRS;
+	private String createDate;
+	private String endDate;
+	private String sZRS;
+	private String zJJJSS;
+	private String yJ1;
+	private String yJ2;
+	private String sWRS;
+	
+}
